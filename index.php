e77ed7c65302d477a55ca4871bec8348799beedf
ProMac Recepción 2 <promacrecepcion2@ProMac-Recepcion-2.local>
2021-03-05 13:35:15 +0000

    Some improveds

--- index.php
+++ index.php
@@ -51,11 +51,10 @@
             {
         ?>
             <tr>
-                <td><?php echo ($row['hora']);?></td>
+                <th scope="col"><?php echo ($row['hora']);?></th>
                 <td><button type='button' class='btn btn-outline-dark' data-bs-toggle='modal' data-bs-target='#exampleModal' data-bs-whatever='" + el + "'>Llamar</button></td>
                 <td><button type='button' class='btn btn-outline-secondary' data-bs-toggle='modal' data-bs-target='#exampleModal' data-bs-whatever='Sin cita por orden de llegada'>Sin cita</button></td>
-                <td><button type='button' class='btn btn-outline-danger'>Eliminar</button></td>
-
+                <td><a class="btn btn-outline.danger" value="Eliminar" href="index.php? id=<?php echo $row["hora"]; ?>"> Eliminar</a></td>
             </tr>
         <?php
             }
@@ -64,8 +63,6 @@
         </table>
 
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="scripts/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
     <script>
