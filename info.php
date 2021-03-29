0e784a830123a93e6b3ea9fa13c5acd02335f38d
ProMac Recepción 2 <promacrecepcion2@ProMac-Recepcion-2.local>
2021-03-29 17:07:30 +0100

    improveds

--- info.php
+++ info.php
@@ -64,8 +64,8 @@
 var contenido = document.querySelector('#example');
 var contenido_2 = document.querySelector('#example_2');
 
-    var myTimer = window.setInterval(traer, 8000);
-    var myTimer2 = window.setInterval(traer2, 9000);
+    var myTimer = window.setInterval(traer, 6000);
+    var myTimer2 = window.setInterval(traer2, 6000);
 
             function traer(){
 
@@ -87,15 +87,15 @@ var contenido_2 = document.querySelector('#example_2');
                 }
             }
 
-function traer2(){
+function traer_2(){
 
 fetch('./data_info_2.php')
 .then(res => res.json())
-.then(data => {alerta2(data)});
+.then(data => {alerta_2(data)});
 
 }
 
-function alerta2 (data){
+function alerta_2 (data){
 
 if(data != ''){
     data.forEach(element => contenido_2.innerHTML = '<h1 class="h1 text-white">Recepción 2</h1><br><h2 class="h2">SU TURNO: </h2> <br> <h3 class="h3">Atendiendo cita: ' + element.hora.toString() + '</h3><p>Recuerde haber realizado copia de seguridad anteriormente.</p>');
@@ -109,7 +109,7 @@ if(data != ''){
 
 traer();
 
-traer2();
+traer_2();
 
 </script>
 </body>
