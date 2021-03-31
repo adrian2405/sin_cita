de9380827ffd4f588c773180d859bd91cb7a33f0
ProMac Recepción 2 <promacrecepcion2@ProMac-Recepcion-2.local>
2021-03-31 15:10:41 +0100

    voice test

--- info.php
+++ info.php
@@ -79,7 +79,7 @@ var contenido_2 = document.querySelector('#example_2');
 
                 if(data != ''){
                     data.forEach(element => contenido.innerHTML = '<h1 class="h1 text-white">Recepción 1</h1><br><h2 class="h2">SU TURNO: </h2> <br> <h3 class="h3">Atendiendo cita: ' + element.hora.toString() + '</h3><p>Recuerde haber realizado copia de seguridad anteriormente.</p>');
-                    data.forEach(element => speak(element.hora.toString()));
+                    data.forEach(element => speak('Su turno, ' + element.hora.toString()));
                 }else{
 
                   contenido.innerHTML = '<h1 class="h1">Recepción 1</h1><br><h2 class="h2">Espere su turno por favor.</h2>';
@@ -99,7 +99,7 @@ function alerta_2 (data){
 
 if(data != ''){
     data.forEach(element => contenido_2.innerHTML = '<h1 class="h1 text-white">Recepción 2</h1><br><h2 class="h2">SU TURNO: </h2> <br> <h3 class="h3">Atendiendo cita: ' + element.hora.toString() + '</h3><p>Recuerde haber realizado copia de seguridad anteriormente.</p>');
-    data.forEach(element => speak(element.hora.toString()));
+    data.forEach(element => speak('Su turno, ' + element.hora.toString()));
 }else{
 
   contenido_2.innerHTML = '<h1 class="h1">Recepción 2</h1><br><h2 class="h2">Espere su turno por favor.</h2>';
@@ -112,10 +112,10 @@ traer();
 traer_2();
 
 function speak (message) {
-  var msg = new SpeechSynthesisUtterance(message)
-  var voices = window.speechSynthesis.getVoices()
-  msg.voice = voices[0]; // Note: some voices don't support altering params
-  window.speechSynthesis.speak(msg)
+  var msg = new SpeechSynthesisUtterance(message);
+  var voices = window.speechSynthesis.getVoices();
+  msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Monica'; })[0];
+  window.speechSynthesis.speak(msg);
 }
 
 </script>
